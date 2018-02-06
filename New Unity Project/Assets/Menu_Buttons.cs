8119f9aaf92eabf80aa77f62cd1ac278555f58a4
SophiaLynch <sophiamarielynch@yahoo.com>
2018-02-05 22:37:22 -0500

    First commit

---
+++ New Unity Project/Assets/Menu_Buttons.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Buttons : MonoBehaviour {
+
+	// game objects needed
+	public GameObject MenuPanel;
+	public GameObject StartPanel;
+	public GameObject QuitPanel;
+
+	// initialization
+	void Start () {
+		MenuPanel.SetActive (true);
+		StartPanel.SetActive (false);
+		QuitPanel.SetActive (false);
+	}
+
+	// public functions for activation of panels
+	public void ShowMenuPanel()
+	{
+		MenuPanel.SetActive (true);
+		StartPanel.SetActive (false);
+		QuitPanel.SetActive (false);
+	}
+
+	public void ShowStartPanel()
+	{
+		MenuPanel.SetActive (false);
+		StartPanel.SetActive (true);
+		QuitPanel.SetActive (false);
+	}
+
+	public void ShowQuitPanel()
+	{
+		MenuPanel.SetActive (false);
+		StartPanel.SetActive (false);
+		QuitPanel.SetActive (true);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
